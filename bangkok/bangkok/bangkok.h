90d8b568ccfed3eb9a14af235083c8f049a1680f
Measmonysuon <measmonysuon@yahoo.com>
2023-07-11 21:39:05 -0700

    Source code

---
+++ bangkok/bangkok/bangkok.h
@@ -0,0 +1,18 @@
+//
+//  bangkok.h
+//  bangkok
+//
+//  Created by Mac Studio on 11/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for bangkok.
+FOUNDATION_EXPORT double bangkokVersionNumber;
+
+//! Project version string for bangkok.
+FOUNDATION_EXPORT const unsigned char bangkokVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bangkok/PublicHeader.h>
+
+
